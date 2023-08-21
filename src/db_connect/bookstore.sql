fc6fdabad88c258a67e1efa75e6ad2aaf57575e4
Taimur Rahman <taimurrahman98@gmail.com>
2023-08-21 06:24:01 +0600

    updated database

--- src/db_connect/bookstore.sql
+++ src/db_connect/bookstore.sql
@@ -1,78 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2023 at 02:23 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
 CREATE TABLE `cart` (
   `customer_id` varchar(11) NOT NULL,
   `item_id` int(11) NOT NULL,
   `quantity` int(11) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table `user`(
-id varchar(11) primary key not null,
-name varchar(50) not null,
-password varchar(255) not null,
-admin tinyint(1) not null DEFAULT 0
-);
+--
+-- Dumping data for table `cart`
+--
 
-create table `item`(
-item_id int(11) not null primary key AUTO_INCREMENT,
-item_name varchar(50) not null,
-item_price int(11) not null,
-stock int(11) not null,
-category varchar(50) not null,
-image_url varchar(255) null DEFAULT NULL,
-image blob null DEFAULT NULL
-);
+INSERT INTO `cart` (`customer_id`, `item_id`, `quantity`) VALUES
+('5730e80ba27', 2, 1),
+('5730e80ba27', 4, 1),
+('5730e80ba27', 8, 1),
+('5730e80ba27', 12, 1);
 
-create table `orders`(
-order_id int(11) not null primary key AUTO_INCREMENT,
-customer_id varchar(11) not null,
-paid tinyint(1) not null DEFAULT 0
-);
+-- --------------------------------------------------------
 
-create table `print`(
-    document_id int(11) not null primary key AUTO_INCREMENT,
-    document blob not null,
-    order_id int(11) not null
-);
+--
+-- Table structure for table `item`
+--
 
-create table `order_item`(
-order_id int(11) not null,
-item_id int(11) not null,
-quantity int(11) not null
-);
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(50) NOT NULL,
+  `item_price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `image` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `item`
+--
 
-alter table cart
-add primary key(customer_id, item_id),
-add foreign key(customer_id) references user(id),
-add foreign key(item_id) references item(item_id);
+INSERT INTO `item` (`item_id`, `item_name`, `item_price`, `stock`, `category`, `image_url`, `image`) VALUES
+(1, 'Lord of the Flies', 100, 10, 'Book', NULL, NULL),
+(2, 'Wuthering Heights', 120, 10, 'Book', NULL, NULL),
+(3, 'Matador Pen', 10, 100, 'Stationery', NULL, NULL),
+(4, 'Sharpener', 20, 100, 'Stationery', NULL, NULL),
+(5, 'Pencil', 5, 100, 'Stationery', NULL, NULL),
+(6, 'Eraser', 10, 100, 'Stationery', NULL, NULL),
+(7, 'Ruler', 10, 100, 'Stationery', NULL, NULL),
+(8, 'A4 Paper', 10, 100, 'Stationery', NULL, NULL),
+(9, 'A3 Paper', 20, 100, 'Stationery', NULL, NULL),
+(10, 'A2 Paper', 30, 100, 'Stationery', NULL, NULL),
+(11, 'A1 Paper', 40, 100, 'Stationery', NULL, NULL),
+(12, 'A0 Paper', 50, 100, 'Stationery', NULL, NULL),
+(13, 'Pygmalion', 200, 5, 'Book', NULL, NULL),
+(14, 'Calculator', 1000, 10, 'Electronics', NULL, NULL);
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `orders`
+--
 
-alter table orders
-add foreign key(customer_id) references user(id);
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `customer_id` varchar(11) NOT NULL,
+  `paid` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-alter table order_item
-add foreign key(order_id) references orders(order_id),
-add foreign key(item_id) references item(item_id);
+-- --------------------------------------------------------
 
-alter table print
-add foreign key(order_id) references orders(order_id);
+--
+-- Table structure for table `order_item`
+--
 
+CREATE TABLE `order_item` (
+  `order_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-insert into user(id, name, password, admin) values
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `print`
+--
+
+CREATE TABLE `print` (
+  `document_id` int(11) NOT NULL,
+  `document` blob NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` varchar(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `admin`) VALUES
+('156d02e0705', 'test2', '123', 0),
+('5730e80ba27', 'test', '123', 0),
 ('admin', 'admin', 'admin', 1);
 
-insert into item(item_name, item_price, stock, category) values
-('Lord of the Flies', 100, 10, 'Book'),
-('Wuthering Heights', 120,10,'Book'),
-('Matador Pen', 10,100,'Stationery'),
-('Sharpener',20,100,'Stationery'),
-('Pencil',5,100,'Stationery'),
-('Eraser',10,100,'Stationery'),
-('Ruler',10,100,'Stationery'),
-('A4 Paper',10,100,'Stationery'),
-('A3 Paper',20,100,'Stationery'),
-('A2 Paper',30,100,'Stationery'),
-('A1 Paper',40,100,'Stationery'),
-('A0 Paper',50,100,'Stationery'),
-('Pygmalion',200,5,'Book'),
-('Calculator',1000,10,'Electronics');
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`customer_id`,`item_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `print`
+--
+ALTER TABLE `print`
+  ADD PRIMARY KEY (`document_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `print`
+--
+ALTER TABLE `print`
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `order_item_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
+
+--
+-- Constraints for table `print`
+--
+ALTER TABLE `print`
+  ADD CONSTRAINT `print_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

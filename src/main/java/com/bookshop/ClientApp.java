5ed3a448cdb395476df0d22f5bd47199f9fe037f
Taimur Rahman <taimurrahman98@gmail.com>
2023-08-26 06:32:35 +0600

    preliminary work for server and client

--- src/main/java/com/bookshop/ClientApp.java
+++ src/main/java/com/bookshop/ClientApp.java
@@ -4,9 +4,41 @@ import com.bookshop.Models.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientApp extends Application {
     @Override
     public void start(Stage stage){
         Model.getInstance().getViewsFactory().showLoginWindow();
+
+        Thread clientThread = new Thread(new ClientThread());
+        clientThread.start();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+class ClientThread implements Runnable {
+    @Override
+    public void run() {
+        try (Socket socket = new Socket("localhost", 8888);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Server: " + in.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/bookshop/ServerApp.java
+++ src/main/java/com/bookshop/ServerApp.java
@@ -4,11 +4,62 @@ import com.bookshop.Models.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp extends Application {
     @Override
     public void start(Stage stage){
         Model.getInstance().setAdminFlag(true);
         Model.getInstance().getViewsFactory().showStoreFrontPage();
+
+        try (ServerSocket serverSocket = new ServerSocket(8888)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                // Handle client communication in a separate thread
+                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
 
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                // Process input from client and send response
+                String response = processInput(inputLine);
+                out.println(response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String processInput(String input) {
+        // Process client input and generate response
+        return "Server says: " + input;
+    }
+}
